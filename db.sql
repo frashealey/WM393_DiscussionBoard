35661f9d567209c8b415472698d9f3746f61f361
frashealey <u1928864@live.warwick.ac.uk>
2022-01-11 15:56:36 +0000

    finish discussion unit test

--- db.sql
+++ db.sql
@@ -1,7 +1,7 @@
 -- this file is used to document development of the PostgreSQL database
 -- developed by 1928864
 
--- creates discusison database, ensures it has been created, and connecting to it
+-- creates discusison database, ensures it has been created, and connects to it
 CREATE DATABASE discussion;
 \l
 \c discussion
@@ -19,8 +19,12 @@ CREATE TABLE uni_user (
     utype BYTEA NOT NULL CHECK (Encode(Decrypt(utype, 'discKey192', 'bf'), 'escape')::CHAR(1) IN ('s', 't'))
 );
 -- inserting example users into uni_user
-INSERT INTO uni_user (id, pw, fname, lname, email, utype) VALUES ('u2139948', Crypt('testPass123', gen_salt('bf', 8)), Encrypt('John', 'discKey192', 'bf'), Encrypt('Smith', 'discKey192', 'bf'), Encrypt('john.smith@warwick.ac.uk', 'discKey192', 'bf'), Encrypt('t', 'discKey192', 'bf'));
-INSERT INTO uni_user (id, pw, fname, lname, email, utype) VALUES ('u1827746', Crypt('testPass123', gen_salt('bf', 8)), Encrypt('Jerry', 'discKey192', 'bf'), Encrypt('Seinfeld', 'discKey192', 'bf'), Encrypt('jerry.seinfeld@warwick.ac.uk', 'discKey192', 'bf'), Encrypt('s', 'discKey192', 'bf'));
+INSERT INTO uni_user (id, pw, fname, lname, email, utype)
+VALUES
+    ('u2139948', Crypt('testPass123', gen_salt('bf', 8)), Encrypt('John', 'discKey192', 'bf'), Encrypt('Smith', 'discKey192', 'bf'), Encrypt('john.smith@warwick.ac.uk', 'discKey192', 'bf'), Encrypt('t', 'discKey192', 'bf'));
+INSERT INTO uni_user (id, pw, fname, lname, email, utype)
+VALUES
+    ('u1827746', Crypt('testPass123', gen_salt('bf', 8)), Encrypt('Jerry', 'discKey192', 'bf'), Encrypt('Seinfeld', 'discKey192', 'bf'), Encrypt('jerry.seinfeld@warwick.ac.uk', 'discKey192', 'bf'), Encrypt('s', 'discKey192', 'bf'));
 
 -- link_user
 CREATE TABLE link_user (
@@ -29,8 +33,8 @@ CREATE TABLE link_user (
     CONSTRAINT not_equal CHECK (lnk_tut_id != lnk_stu_id),
     PRIMARY KEY (lnk_tut_id, lnk_stu_id)
 );
--- trigger to check user role
-CREATE OR REPLACE FUNCTION func_link_role_check() RETURNS trigger AS
+-- trigger to disallow incorrect user types
+CREATE OR REPLACE FUNCTION func_link_type_check() RETURNS trigger AS
 $$
     DECLARE
         tutor_type CHAR(1);
@@ -48,4 +52,40 @@ $$
         RETURN NEW;
     END;
 $$ LANGUAGE 'plpgsql';
-CREATE TRIGGER trig_link_role_check AFTER INSERT OR UPDATE OR DELETE ON link_user FOR EACH ROW EXECUTE PROCEDURE func_link_role_check();
+CREATE TRIGGER trig_link_type_check AFTER INSERT OR UPDATE OR DELETE ON link_user FOR EACH ROW EXECUTE PROCEDURE func_link_type_check();
+-- inserting example entries into link user
+INSERT INTO link_user (lnk_tut_id, lnk_stu_id)
+VALUES
+    ('u2139948', 'u1827746');
+
+-- discussion board
+-- (function to enforce 1 non-archived board to be outlined in js)
+CREATE TABLE discussion (
+    dis_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    dis_owner CHAR(8) REFERENCES uni_user(id),
+    dis_title VARCHAR(50) NOT NULL,
+    archived BOOLEAN NOT NULL DEFAULT false
+);
+-- trigger to ensure only tutors own discussion boards
+CREATE OR REPLACE FUNCTION func_discussion_type_check() RETURNS trigger AS
+$$
+    DECLARE
+        user_type CHAR(1);
+    BEGIN
+        user_type = (SELECT Encode(Decrypt(utype, 'discKey192', 'bf'), 'escape')::CHAR(1) as utype FROM uni_user WHERE id=NEW.dis_owner);
+
+        IF (user_type != 't')
+            THEN RAISE EXCEPTION 'User must be tutor to own discussion board';
+        END IF;
+
+        RETURN NEW;
+    END;
+$$ LANGUAGE 'plpgsql';
+CREATE TRIGGER trig_discussion_type_check AFTER INSERT OR UPDATE OR DELETE ON discussion FOR EACH ROW EXECUTE PROCEDURE func_discussion_type_check();
+
+INSERT INTO discussion (dis_owner, dis_title, archived)
+VALUES
+    ('u2139948', 'Example discussion board', false);
+INSERT INTO discussion (dis_owner, dis_title, archived)
+VALUES
+    ('u1827746', 'Example discussion board', false);
--- test_db.sql
+++ test_db.sql
@@ -21,6 +21,13 @@ INSERT INTO uni_user (id, pw, fname, lname, email, utype) VALUES ('u2139948', Cr
 TRUNCATE TABLE uni_user;
 
 -- unit test 2 - verifying link_user constraints/functions
+-- setup - inserting example users into uni_user
+INSERT INTO uni_user (id, pw, fname, lname, email, utype)
+VALUES
+    ('u2139948', Crypt('testPass123', gen_salt('bf', 8)), Encrypt('John', 'discKey192', 'bf'), Encrypt('Smith', 'discKey192', 'bf'), Encrypt('john.smith@warwick.ac.uk', 'discKey192', 'bf'), Encrypt('t', 'discKey192', 'bf'));
+INSERT INTO uni_user (id, pw, fname, lname, email, utype)
+VALUES
+    ('u1827746', Crypt('testPass123', gen_salt('bf', 8)), Encrypt('Jerry', 'discKey192', 'bf'), Encrypt('Seinfeld', 'discKey192', 'bf'), Encrypt('jerry.seinfeld@warwick.ac.uk', 'discKey192', 'bf'), Encrypt('s', 'discKey192', 'bf'));
 -- 2.1 - null lnk_tut_id
 INSERT INTO link_user (lnk_tut_id, lnk_stu_id) VALUES (NULL, 'u2139948');
 -- 2.2 - null lnk_stu_id
@@ -33,3 +40,7 @@ INSERT INTO link_user (lnk_tut_id, lnk_stu_id) VALUES ('u1827746', 'u2139948');
 INSERT INTO link_user (lnk_tut_id, lnk_stu_id) VALUES ('u2139948', 'u1827746');
 -- reset after testing
 TRUNCATE TABLE link_user;
+TRUNCATE TABLE uni_user;
+
+-- unit test 3 - verifying discussion constraints
+-- 
