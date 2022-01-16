ce5b39a75e52eb12c62c34ff965a80087c5162c0
unknown <fraser42a@gmail.com>
2022-01-16 23:05:31 +0000

    figure out encrypt/decrypt

--- app.js
+++ app.js
@@ -6,15 +6,16 @@ npm install ejs
 
 const express = require("express"),
       path = require("path"),
-    //   { Pool } = require("pg"),
+      { Pool } = require("pg"),
+      pool1 = new Pool({
+        host: "localhost",
+        port: 5432,
+        user: "pool1",
+        password: "pool1pass",
+        database: "discussionboard"
+      }),
+      crypto = require("crypto"),
       port = process.env.PORT || 3000,
-    //   dbCreds = {
-    //                 user: "pool1",
-    //                 host: "localhost",
-    //                 database: "discussionboard",
-    //                 password: "pool1pass",
-    //                 port: 5432,
-    //             },
       server = express();
 // set view engine to ejs
 server.set("view engine", "ejs");
@@ -23,6 +24,10 @@ server.use(express.static(path.join(__dirname, "public")));
 server.get("/", async (req, res) => {
     // res.statusCode = 200;
     // res.writeHead(200, {"Content-Type": "text/html"});
+    const testResults = await pool1.query("SELECT id, fname, lname, utype FROM uni_user;");
+    // var decipher = crypto.createDecipheriv("bf-cbc", "discKey192");
+        // dec = Buffer.concat([decipher.update(testResults.rows[0].fname) , decipher.final()]);
+    console.log(testResults.rows);
     res.render("home");
 });
 
