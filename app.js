169d4846d00306d4b9381177186b67dab0ec501e
unknown <fraser42a@gmail.com>
2022-01-16 15:31:14 +0000

    removed hostname var

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ npm install ejs
 const express = require("express"),
       path = require("path"),
     //   { Pool } = require("pg"),
-      hostname = "localhost",
       port = process.env.PORT || 3000,
     //   dbCreds = {
     //                 user: "pool1",
@@ -19,17 +18,14 @@ const express = require("express"),
       server = express();
 // set view engine to ejs
 server.set("view engine", "ejs");
-// server.use(express.static(__dirname));
 server.use(express.static(path.join(__dirname, "public")));
 
 server.get("/", async (req, res) => {
     // res.statusCode = 200;
     // res.writeHead(200, {"Content-Type": "text/html"});
-
     res.render("home");
 });
 
-// server.listen(process.env.PORT || 3000, hostname, () => {
 server.listen(port, () => {
-    console.log(`Server running at http://${hostname}:${port}`);
+    console.log(`Server running on port: ${port}`);
 });
