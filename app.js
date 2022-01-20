b6df86f2f08758ccf0bca63efc226a2203d08e85
unknown <fraser42a@gmail.com>
2022-01-20 18:27:43 +0000

    installed bodyParser

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express"),
+      bodyParser = require("body-parser"),
       path = require("path"),
       { Pool } = require("pg"),
       pool1 = new Pool({
@@ -10,9 +11,11 @@ const express = require("express"),
       }),
       port = process.env.PORT || 3000,
       server = express();
-// set view engine to ejs
+// declaring view engine (as ejs), folder structure, and bodyParser
 server.set("view engine", "ejs");
 server.use(express.static(path.join(__dirname, "public")));
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: false }));
 
 server.get("/", async (req, res) => {
     // res.statusCode = 200;
@@ -30,12 +33,13 @@ server.get("/", async (req, res) => {
         throw e;
     };
 
-    res.render("register");
+    res.render("login");
     // res.render("discussion", {x: y});
 });
 
 server.post("/login", async (req, res) => {
-    console.log(req);
+    console.log(req.body.id);
+    console.log(req.body.pw);
 });
 
 server.post("/register", async (req, res) => {
