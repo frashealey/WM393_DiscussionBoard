9b6bed6788c972e39e734fbf7a48fe27940bf00c
unknown <fraser42a@gmail.com>
2022-01-24 14:18:57 +0000

    getting client passed null/undefined query error?

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const { initialize } = require("passport");
+const { execPath } = require("process");
+
 const express = require("express"),
       bodyParser = require("body-parser"),
       path = require("path"),
@@ -22,8 +25,30 @@ server.use(express.static(path.join(__dirname, "public")));
 // declaring bodyParser (e.g. to read data from forms)
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: false }));
+server.use(flash());
 
-server.get("/", async (req, res) => {
+
+
+
+// declaring passport
+passportInit(passport);
+server.use(
+    session({
+        // this would normally be placed in .env with .gitignore,
+        // but for purposes of assessment it is here
+        secret: "discussSession192192",
+        resave: false,
+        saveUninitialized: false
+    })
+);
+server.use(passport.initialize());
+server.use(passport.session());
+
+
+
+
+
+server.get("/", checkNotAuthenticated, async (req, res) => {
     // try {
     //     // const testResults = await pool1.query("SELECT id, pw, fname, lname, utype FROM uni_user;");
     //     // const testResults = await pool1.query("SELECT Crypt('testPass123', gen_salt('md5'));");
@@ -36,22 +61,28 @@ server.get("/", async (req, res) => {
     //     throw e;
     // };
 
-    res.redirect("/register");
-    // res.render("discussion");
+    console.log(req.user);
+    res.render("discussion");
 });
 
-server.get("/login", async (req, res) => {
+server.get("/login", checkAuthenticated, async (req, res) => {
     res.render("login");
 });
-server.post("/login", async (req, res) => {
-    const logCreds = {
-        id: req.body.id,
-        pw: req.body.pw,
-    };
-    console.log(logCreds);
-});
+// server.post("/login", async (req, res) => {
+//     const logCreds = {
+//         id: req.body.id,
+//         pw: req.body.pw,
+//     };
+//     console.log(logCreds);
+// });
+server.post("/login", passport.authenticate("local", {
+        successRedirect: "/discussion",
+        failureRedirect: "/login",
+        failureFlash: true
+    })
+);
 
-server.get("/register", async (req, res) => {
+server.get("/register", checkAuthenticated, async (req, res) => {
     res.render("register");
 });
 server.post("/register", async (req, res) => {
@@ -65,7 +96,7 @@ server.post("/register", async (req, res) => {
             confpw: req.body.confpw,
             ...(Boolean(req.body.utype) ? { utype: "s" } : { utype: "t" })
         },
-            regExists = await pool1.query(`SELECT id, email, pw FROM uni_user WHERE id=$1 OR email=Encrypt($2, 'discussKey192192', 'aes');`, [regCreds.id, regCreds.email]);
+              regExists = await pool1.query(`SELECT id, email, pw FROM uni_user WHERE id=$1 OR email=Encrypt($2, 'discussKey192192', 'aes');`, [regCreds.id, regCreds.email]);
 
         // (don't need to check utype, as this 'selected' by default)
         if (!regCreds.id || !regCreds.fname || !regCreds.lname || !regCreds.email || !regCreds.pw || !regCreds.confpw) {
@@ -80,12 +111,13 @@ server.post("/register", async (req, res) => {
         // register user
         else {
             const client = await pool1.connect();
+            let regSuccess = false;
             try {
                 await client.query("BEGIN");
                 await client.query(`INSERT INTO uni_user (id, pw, fname, lname, email, utype) VALUES ($1, Crypt($2, gen_salt('md5')), Encrypt($3, 'discussKey192192', 'aes'), Encrypt($4, 'discussKey192192', 'aes'), Encrypt($5, 'discussKey192192', 'aes'), Encrypt($6, 'discussKey192192', 'aes'));`,
                                    [regCreds.id, regCreds.pw, regCreds.fname, regCreds.lname, regCreds.email, regCreds.utype]);
                 await client.query("COMMIT");
-                res.redirect("/login");
+                regSuccess = true;
             }
             catch(e) {
                 let tempInvalidMsg;
@@ -105,6 +137,7 @@ server.post("/register", async (req, res) => {
             }
             finally {
                 client.release();
+                if (regSuccess) { res.redirect("/login") };
             };
         };
     }
@@ -116,9 +149,9 @@ server.post("/register", async (req, res) => {
 });
 
 server.post("/logout", (req, res) => {
+    console.log("LOGOUT");
     // req.logout();
     // res.redirect("/login");
-    console.log("LOGOUT");
 });
 
 // redirect undefined pages to home page
@@ -129,13 +162,82 @@ server.get("*", function(req, res) {
 
 
 
-// function passportInit(passport) {   
 
-// };
-// function userAuth() {
+function passportInit(passport) {
+    passport.use(new LocalStrategy({ usernameField: "id", passwordField: "pw" }, userAuth));
 
-// };
+    passport.serializeUser((user, done) => done(null, user.id));
+    passport.deserializeUser(async (id, done) => {
+        try {
+            const findUser = pool1.query(`SELECT id, pw, fname, lname, email, utype FROM users WHERE id=$1;`, [id]);
+            return done(null, findUser.rows[0]);
+        }
+        catch(e) {
+            return done(e);
+        };
+    });
+};
 
+async function userAuth(id, pw, done) {
+    console.log(id, pw);
+    // const pwCheck = await pool1.query(`SELECT * FROM uni_user WHERE id=$1::VARCHAR;` [id]);
+    // console.log(pwCheck.rows);
+
+
+
+
+
+
+
+    // try {
+    //     const idCheck = await pool1.query(`SELECT id FROM uni_user WHERE id=$1;` [id]);
+    //     // password check
+    //     if (idCheck.rows.length === 1) {
+    //         try {
+    //             const pwCheck = await pool1.query(`SELECT id, pw FROM uni_user WHERE id=$1 AND pw=Crypt($2, pw);` [id, pw]);
+    //             if (pwCheck.rows.length === 1) {
+    //                 console.log("SUCCESS");
+    //                 return done(null, pwCheck.rows[0]);
+    //             }
+    //             else {
+    //                 console.log("Password incorrect");
+    //                 return done(null, false, {message: "Password is incorrect"});
+    //             };
+    //         }
+    //         catch(e) {
+    //             console.log(e);
+    //             res.render("login", {message: "Unknown error - please try again"});
+    //         };
+    //     }
+    //     // no user found
+    //     else {
+    //         console.log("No user with that ID");
+    //         return done(null, false, {message: "No user with that ID found"});
+    //     };
+    // }
+    // // idCheck error
+    // catch(e) {
+    //     console.log(e);
+    //     res.render("login", {message: "Unknown error - please try again"});
+    // };
+};
+
+
+
+
+function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return res.redirect("/discussion");
+    };
+    next();
+};
+
+function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    };
+    res.redirect("/login");
+};
 
 
 
